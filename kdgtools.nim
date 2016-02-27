f4deb524723a9164e5b048be0ab055d23b5aed07
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2016-02-26 22:04:26 -0700

    Fix hoisting code

--- kdgtools.nim
+++ kdgtools.nim
@@ -1,24 +1,90 @@
 import sequtils
 import kdg
 
+# inverse edge types.
+# for example, inverse of "agent" is "performed_action"
+const
+  inverseEdgeTypes =
+    {"agent":          "performed_action",
+     "objective":      "required_for",
+     "next_event":     "previous_event",
+     "instance_of":    "has_instance",
+     "is_subclass_of": "has_subclass",
+     "modifier":       "modifies"}
+
+proc invertEdge(edge: string): string =
+  for pair in inverseEdgeTypes:
+    let (k, v) = pair
+    if edge == k: return v
+    if edge == v: return k
+
+  if edge[0] == '!':
+    return edge[1..^1]
+  else:
+    return "!" & edge
+
 # A simple transformation. This takes any node in the tree and makes it the
 # root node of the tree.
 
-proc hoistNode*(kgraph: KDG, node: KNode, newEdge: string): KDG =
-  var searchQueue: seq[tuple[parent: KDG, child: KDG]] = @[(kgraph, kgraph)]
-  while searchQueue.len > 0:
-    let (curParent, curChild) = searchQueue.pop()
-    if curChild.node == node:
-      curParent.children.keepItIf(it.to.node != node)
-      curChild.children.add((newEdge, kgraph))
-      return curChild
+proc hoistNodeSingleDepth(kgraph: KDG, node: KNode, newEdge: string = nil):
+                          tuple[success: bool, kgraph: KDG] =
+  var childToHoist: KDG = nil
+  var edge = newEdge
+  result.success = false
 
-    for child in curChild.children:
-      searchQueue.add((curChild, child.to))
+  if kgraph.node == node:
+    result.success = true
+    result.kgraph = kgraph
+    return
 
-proc hoistAgent*(kgraph: KDG): KDG =
+  for child in kgraph.children:
+    let childGraph = child.to
+    if childGraph.node == node:
+      result.success = true
+      childToHoist = childGraph
+      if isNil(edge):
+        edge = invertEdge(child.edgeType)
+
+  if result.success:
+    kgraph.children.keepItIf(it.to.node != node)
+
+    childToHoist.children.add((edge, kgraph))
+    result.kgraph = childToHoist
+
+proc findPathTo(kgraph: KDG, node: KNode): tuple[found: bool, path: seq[KNode]] =
+  result.found = false
+  let current = kgraph.node
+  result.path = @[current]
+  if kgraph.node == node:
+    result.found = true
+    return
+
+  for child in kgraph.children:
+    let (childEdge, childGraph) = child
+    let (childHasPath, path) = childGraph.findPathTo(node)
+    if childHasPath:
+      result.found = true
+      result.path.add(path)
+      return
+
+proc hoistNode*(kgraph: KDG, node: KNode): tuple[success: bool, kgraph: KDG] =
+  let (found, path) = kgraph.findPathTo(node)
+  if not found:
+    return (false, nil)
+
+  var currentGraph = kgraph
+  for step in path:
+    let (success, newGraph) = currentGraph.hoistNodeSingleDepth(step)
+    if success:
+      currentGraph = newGraph
+    else:
+      return (false, nil)
+
+  return (true, currentGraph)
+
+proc hoistAgent*(kgraph: KDG): tuple[success: bool, kgraph: KDG] =
   for child in kgraph.children:
     if child.edgeType == "agent":
-      return kgraph.hoistNode(child.to.node, "performed_action")
+      return kgraph.hoistNode(child.to.node)
 
-  return kgraph
+  return (false, kgraph)
--- simpleclient.nim
+++ simpleclient.nim
@@ -2,11 +2,10 @@ import os, asyncdispatch
 import kdg, kclient, kdgtools
 
 let params = commandLineParams()
-let appFilename = paramStr(0)
 if params.len < 1:
-  echo "use it like this: " & appFileName & " \"Who killed Abraham Lincoln?\""
+  echo "usage: simpleclient \"Who killed Abraham Lincoln?\""
 else:
   let sentence = params[0]
   let kdgs = waitFor kparse(sentence)
   echo "Here is the agent-hoisted KDG for \"" & sentence & "\""
-  for kg in kdgs: echo kg.hoistAgent()
+  for kg in kdgs: echo kg.hoistAgent().kgraph
